1978b3546a75948ea488bf1388013f2bc1b35d7d
Shrey Modi <45357209+shrey1608@users.noreply.github.com>
2020-09-12 09:50:58 +0530

    Update settings.py

--- donatetoartsy/settings.py
+++ donatetoartsy/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2vc8#$fzhh-z9h(-(5kl2kj)ijq-1_(s0j5g4&)+9z%0t30x-1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'donateforartsy.herokuapp.com']
+ALLOWED_HOSTS = ['', '']
 
 
 # Application definition
